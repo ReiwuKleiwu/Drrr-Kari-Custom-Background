cdb1c8fed1f81e29caa4ae9d10f28256507988d6
ReiwuKleiwu <xxstruckixx@gmail.com>
2019-03-01 16:20:28 +0100

    Add files via upload

---
+++ DrrrKariBackground.js
@@ -0,0 +1,12 @@
+"use strict"; 
+
+window.addEventListener("load", function(){
+
+    let body = document.body;
+    body.style.background = "url('https://pixelz.cc/wp-content/uploads/2017/01/view-of-mout-fuji-from-red-pagoda-tkyo-japan-uhd-4k-wallpaper.jpg')";
+    body.style.backgroundSize = "cover";
+    body.style.backgroundAttachment = "fixed";
+    body.style.backgroundPosition = "center center"; 
+    body.style.backgroundRepeat ="repeat-y"; 
+
+}, false); 
